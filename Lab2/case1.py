d6451240ebfcaf45adfd53701af335fdfc30f052
Karol Błędziński <kbledzinski@gmail.com>
2018-12-09 15:39:12 +0100

    Porpawki do case 1

--- Lab2/case1.py
+++ Lab2/case1.py
@@ -3,3 +3,7 @@ surname = input("What is your your surname?")
 location = input("What is your your location?")
 age = input("What is your your age?")
 print("Hi", name, surname,"! Your location is", location, "and you are", age, "years old.")
+
+# wyjscie = open("uzytkownik.txt", "w")
+# wyjscie.write("asdasd")
+# wyjscie.close()
