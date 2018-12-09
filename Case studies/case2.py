ab75f4ed453f4367e77ab4a0af52e462987004ee
Karol Błędziński <kbledzinski@gmail.com>
2018-12-09 15:50:36 +0100

    Poprawki do case2

--- Case studies/case2.py
+++ Case studies/case2.py
@@ -1,7 +1,12 @@
 # Create a simple IF function that compares two values and prints result.
-nativeVLAN = 1
-dataVLAN = 100
+nativeVLAN = "1"
+dataVLAN = "100"
 
-vlan = input("Podaj VLAN")
+vlan = input("Podaj VLAN: ")
 
-if
+if vlan == nativeVLAN:
+    print("Podany VLAN jest natywnym VLANEM.")
+elif vlan == dataVLAN:
+    print("Podany VLAN jest data VLANEM.")
+else:
+    print("Brak VLANow o tym numerze.")
