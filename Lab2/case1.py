af895b740383aaa3de53374c9dfc14bf90cd68b7
Karol Błędziński <kbledzinski@gmail.com>
2018-12-09 15:36:07 +0100

    Poprawki

---
+++ Lab2/case1.py
@@ -0,0 +1,5 @@
+name = input("What is your your name?")
+surname = input("What is your your surname?")
+location = input("What is your your location?")
+age = input("What is your your age?")
+print("Hi", name, surname,"! Your location is", location, "and you are", age, "years old.")
